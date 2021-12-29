3ec27f8bdfee27c04baf7e6de8e248232d71c08f
whara123 <whara123@gmail.com>
2021-12-29 22:43:31 +0900

    index js fix
    모달창 뜰때 스크롤 방지

--- js/index.js
+++ js/index.js
@@ -3,13 +3,18 @@ const modal =document.querySelector(".modal");
 const modalClose = document.querySelector(".btn-modal");
 const dimd = document.querySelector(".dimd");
 const inputEmail = document.querySelector(".inp-email");
+const bodyScroll = document.querySelector(".body");
+
+
 const reg = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]$/i;
+
 
 subBtn.addEventListener("click",()=>{
     let eMail = inputEmail.value;
     if(eMail.match(reg)!==null){
         dimd.classList.add("modal-on");
         modal.classList.add("modal-on");
+        bodyScroll.classList.add("modal-on");
     }else{
         window.alert("이메일을 입력하세요");
     }
@@ -18,5 +23,5 @@ subBtn.addEventListener("click",()=>{
 modalClose.addEventListener("click",()=>{
     modal.classList.remove("modal-on");
     dimd.classList.remove("modal-on");
-
+    bodyScroll.classList.remove("modal-on");
 })
