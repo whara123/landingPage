6be3452e62e9770b549476450e80f38d92a696eb
whara123 <whara123@gmail.com>
2021-12-29 22:34:16 +0900

    landing page css and js
    전체 style적용
    js 모달창 띄우기
    정규표현식으로 이메일 검사

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const subBtn = document.querySelector(".btn-sub");
+const modal =document.querySelector(".modal"); 
+const modalClose = document.querySelector(".btn-modal");
+const dimd = document.querySelector(".dimd");
+const inputEmail = document.querySelector(".inp-email");
+const reg = /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]$/i;
+
+subBtn.addEventListener("click",()=>{
+    let eMail = inputEmail.value;
+    if(eMail.match(reg)!==null){
+        dimd.classList.add("modal-on");
+        modal.classList.add("modal-on");
+    }else{
+        window.alert("이메일을 입력하세요");
+    }
+});
+
+modalClose.addEventListener("click",()=>{
+    modal.classList.remove("modal-on");
+    dimd.classList.remove("modal-on");
+
+})
